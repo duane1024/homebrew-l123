db417fc7a83ef2a070d3443eb6bb1877f77b9ecc
Duane Moore <duane.moore@ieee.org>
2026-05-09 19:36:14 -0400

    l123 1.2.1

--- Formula/l123.rb
+++ Formula/l123.rb
@@ -1,8 +1,8 @@
 class L123 < Formula
   desc "Terminal clone of Lotus 1-2-3 Release 3.4a"
   homepage "https://github.com/duane1024/l123"
-  url "https://github.com/duane1024/l123/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "ddd1032347a6df62d59efa4466b195db84227a5ac27041a729fb13882d1c1538"
+  url "https://github.com/duane1024/l123/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "176d179b9aa2157d80d572aa8b5c0a8bd8e9dc13a4563a3b13b9ca1c5c471a9d"
   license "MIT"
   head "https://github.com/duane1024/l123.git", branch: "master"
 
